0f3d50692c778bea4ba4b3fed9bbac8c15e4dc88
Lev# <mr.lmartirosyan888@gmail.com>
2019-01-31 21:03:14 +0400

    relise one

---
+++ MathPow/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathPow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pease write your digit");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Now write what degree do you want to count");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Math.Pow(a, b);
+            Console.WriteLine("Your numbre is {0}", c);
+            Console.ReadLine();
+        }
+    }
+    
+}
